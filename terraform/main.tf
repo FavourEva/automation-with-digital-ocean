7cb39492bd245547271139733b8cee93f6c770dd
Onyeocha Favour <97852822+FavourEva@users.noreply.github.com>
2023-09-21 15:01:05 +0000

    enable firewall

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ resource "digitalocean_firewall" "server" {
 
   droplet_ids = digitalocean_droplet.server.*.id
 
-  inbound_rule {
+    inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
     source_addresses = ["192.168.1.0/24", "2002:1:2::/48"]
@@ -79,6 +79,28 @@ resource "digitalocean_firewall" "server" {
     protocol         = "tcp"
     port_range       = "443"
     source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+    outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
 }
